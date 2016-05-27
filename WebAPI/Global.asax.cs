82aaa02f12cf2c0dbbccaac9fe915a798207a7b0
VKabanov <vkabanov1978@gmail.com>
2016-05-27 17:28:38 +0300

    first commit

---
+++ WebAPI/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_BeginRequest()
+        {
+            var value = Context.Request.Url;
+        }
+        protected void Application_EndRequest()
+        {
+            Console.Write(this.Context.AllErrors);
+        }
+
+    }
+}
